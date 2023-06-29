c40bfcb1c9aea6cf7f5ad175907fcb75ee00b4e0
kevin ventura <kevcoder@kevins-mbp.myfiosgateway.com>
2023-06-29 12:11:31 -0400

    wip: server/dependencies added

---
+++ server.js
@@ -0,0 +1,42 @@
+//=====================================
+//         Dependencies
+//=====================================
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const mongoose = require('mongoose')
+
+//=====================================
+//        Database Connection
+//=====================================
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// Database Connection Error/Success
+// Define callback functions for various events
+const db = mongoose.connection
+db.on('error', (err) => console.log(err.message + ' is mongo not running?'));
+db.on('connected', () => console.log('mongo connected'));
+db.on('disconnected', () => console.log('mongo disconnected'));
+
+//=====================================
+//      MIDDLEWARE & BODY PARSER
+//=====================================
+app.use(express.urlencoded({ extended: true }));
+
+
+//=====================================
+//          Routes
+//=====================================
+
+
+
+
+
+//=====================================
+//            Listener
+//=====================================
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`server is listening on port ${PORT}`))
